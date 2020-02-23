1ecab28047423419c0d31a448fc1dbe80ee7076f
YuanJunYao <lvsecoto@gmail.com>
2020-02-23 08:38:31 +0800

    新增表格布局

--- lib/ui/statistics/widget/content_widget.dart
+++ lib/ui/statistics/widget/content_widget.dart
@@ -6,8 +6,33 @@ import 'widget.dart';
 class StatisticsContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: Text("饭量统计")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("表格一览",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: theme.primaryColor,
+              ),
+            ),
+            ListTile(
+              title: Text("abcd"),
+            ),
+            ListTile(
+              title: Text("abcd"),
+            )
+          ],
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
