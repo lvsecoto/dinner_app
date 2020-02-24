e205661959d32b85808663e56d093665f784b7ca
YuanJunYao <lvsecoto@gmail.com>
2020-02-24 19:42:50 +0800

    新增加载页面

--- lib/ui/statistics/widget/talbe_list_widget.dart
+++ lib/ui/statistics/widget/talbe_list_widget.dart
@@ -8,18 +8,22 @@ class TableListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<TableAllController>(
       builder: (__, controller, _) {
+        if (controller.tables == null) {
+          return Center(child: Text("加载中"));
+        }
+
         return ListView(
           children: <Widget>[
             ...controller.tables?.map((e) {
-                  return ListTile(
-                    title: Text(e.name ?? e.index),
-                    onTap: () async {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (_) =>
-                              StatisticsPageWidget(tableIndex: e.index)));
-                    },
-                  );
-                }) ??
+              return ListTile(
+                title: Text(e.name ?? e.index),
+                onTap: () async {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (_) =>
+                          StatisticsPageWidget(tableIndex: e.index)));
+                },
+              );
+            }) ??
                 []
           ],
         );
